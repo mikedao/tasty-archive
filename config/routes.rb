964b7f477197b89a4eee780c4e2c2fee7d0cb26c
Michael Dao <mike@turing.edu>
2022-05-17 13:40:22 -0600

    create user works

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root 'welcome#index'
+  resources :users, only: [:new, :create]
 end
--- spec/features/authentication/visitor_can_create_a_user_spec.rb
+++ spec/features/authentication/visitor_can_create_a_user_spec.rb
@@ -6,15 +6,15 @@ RSpec.describe 'as a visitor when I visit the home page', type: :feature do
 
     click_on 'Sign Up'
 
-    expect(current_path).to eq(new_users_path)
+    expect(current_path).to eq(new_user_path)
 
     username  = 'sej'
     email     = 'sean@example.com'
     password  = 'hamburger1'
 
-    fill_in :username, with: username
-    fill_in :email, with: email
-    fill_in :password, with: password
+    fill_in :user_username, with: username
+    fill_in :user_email, with: email
+    fill_in :user_password, with: password
     
     click_on 'Create User'
 
